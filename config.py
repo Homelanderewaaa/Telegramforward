89263c173c84d02458d63d5827f29ffcbbfe73e7
Homelanderewaaa <124607402+Homelanderewaaa@users.noreply.github.com>
2023-02-07 01:00:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,7 @@ import logging
 class Config:                                                                   
     API_ID = int(os.environ.get("26311402", ""))
     API_HASH = os.environ.get("012450e57d2bc98d0693c6982d01a2d7", "")       
-    BOT_TOKEN = os.environ.get("5868897210:AAGLyP_LOHuX69N6SXYHG0QSgu5Ii7LZe5g
-", "")
+    BOT_TOKEN = os.environ.get("5868897210:AAGLyP_LOHuX69N6SXYHG0QSgu5Ii7LZe5g", "")
     BOT_SESSION = os.environ.get("BQGReuoAULFN8zOauZ-fm7xbu4fdhkfRb61nPGFmVs5_Z5uZM_tFz1pFSIIJ8vm--KFXyPxht4SPs1CEZv4rAXOS3c3zbhzB2m04QuyzF1nvXZL2UQoDKXApcQK4rG_ge8pByi6Q-0FcqPyMwjF1mPcRP2mMyrKNoYhOYTQjaOcACoWgYQaqbooMvx4nozOUXTb21aFUyF91C6tBKzFit2DhWfBupSCp6QBrfs_BhicOSo5oVaqSo20Gxtn7kUC-Kn8crtmSPhXuhLnhJHf0q-cVBi9BwJz-gosARzafbL_lg6saOubCzFIX-pqS4V2bGPlMFLt6pQJVzYzX0HXqZzfOcgEizAAAAAFbqs4nAA", "forwardbot")
     OWNER_ID = os.environ.get("5832887847", "")                             
     DATABASE_URI = os.environ.get("mongodb+srv://Hacker:BREatMXQAWSX8Res@cluster0.0vrauzh.mongodb.net/?retryWrites=true&w=majority", "")  
